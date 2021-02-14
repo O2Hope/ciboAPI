9f6882fa978b690eef7ee3a8703dc9bd9636737d
Agustin Ruiz <agustin.ruiz@ey.com>
2021-02-14 14:54:29 -0600

    Add asyncHandler and errorHandler

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,87 +1,77 @@
 const Restaurant = require("../models/Restaurant");
+const asyncHandler = require("../middleware/async");
+const ErrorResponse = require("../utils/errorResponse");
 
 // @desc    Get all restaurants
 // @route   GET /api/v1/restaurants
 // @access  Public
-exports.getRestaurants = async (req, res, next) => {
-  try {
-    const restaurants = await Restaurant.find();
+exports.getRestaurants = asyncHandler(async (req, res, next) => {
+  const restaurants = await Restaurant.find();
 
-    res.status(200).json({ success: true, count: restaurants.length, data: restaurants });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  res
+    .status(200)
+    .json({ success: true, count: restaurants.length, data: restaurants });
+});
 
 // @desc    Get single restaurants
 // @route   GET /api/v1/restaurants/:id
 // @access  Public
-exports.getRestaurant = async (req, res, next) => {
-  try {
-    const restaurant = await Restaurant.findById(req.params.id);
+exports.getRestaurant = asyncHandler(async (req, res, next) => {
+  const restaurant = await Restaurant.findById(req.params.id);
 
-    if (!restaurant) {
-      return res.status(404).json({ success: false });
-    }
-
-    res.status(200).json({ success: true, data: restaurant });
-  } catch (e) {
-    res.status(400).json({ success: false });
+  if (!restaurant) {
+    return next(
+      new ErrorResponse(`Restaurant not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: restaurant });
+});
 
 // @desc    Create new restaurant
 // @route   POST /api/v1/restaurants
 // @access  Private
-exports.createRestaurant = async (req, res, next) => {
-  try {
-    const restaurant = await Restaurant.create(req.body);
+exports.createRestaurant = asyncHandler(async (req, res, next) => {
+  const restaurant = await Restaurant.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: restaurant,
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: restaurant,
+  });
+});
 
 // @desc    Update restaurant
 // @route   PUT /api/v1/restaurants/:id
 // @access  Private
-exports.updateRestaurant = async (req, res, next) => {
-  try {
-    const restaurant = await Restaurant.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
-
-    if (!restaurant) {
-      return res.status(404).json({ success: false });
+exports.updateRestaurant = asyncHandler(async (req, res, next) => {
+  const restaurant = await Restaurant.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
     }
+  );
 
-    res.status(200).json({ success: true, data: restaurant });
-  } catch (e) {
-    res.status(400).json({ success: false });
+  if (!restaurant) {
+    return next(
+      new ErrorResponse(`Restaurant not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: restaurant });
+});
 
 // @desc    Delete restaurant
 // @route   DELETE /api/v1/restaurants/:id
 // @access  Private
-exports.deleteRestaurant = async (req, res, next) => {
-  try {
-    const restaurant = await Restaurant.findByIdAndDelete(req.params.id);
-    if (!restaurant) {
-      return res.status(404).json({ success: false });
-    }
-
-    res.status(200).json({ success: true, data: {} });
-  } catch (e) {
-    res.status(400).json({ success: false });
+exports.deleteRestaurant = asyncHandler(async (req, res, next) => {
+  const restaurant = await Restaurant.findByIdAndDelete(req.params.id);
+  if (!restaurant) {
+    return next(
+      new ErrorResponse(`Restaurant not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: {} });
+});
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require('morgan')
-const colors = require('colors')
-const connectDB = require('./config/db');
+const morgan = require("morgan");
+const colors = require("colors");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
 
 // Load env variables
 dotenv.config({ path: "./config/config.env" });
@@ -16,25 +17,30 @@ const restaurants = require("./routes/restaurants");
 const app = express();
 
 // Body parser
-app.use(express.json())
+app.use(express.json());
 
 // Dev logging middleware
-if(process.env.NODE_ENV === 'development'){
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev", {}));
 }
 
 // Mount routers
 app.use("/api/v1/restaurants", restaurants);
 
+// Error handling
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.rainbow);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.rainbow
+  );
 });
 
 // Handle unhandled promise rejection
-process.on('unhandledRejection', (err, promise) => {
+process.on("unhandledRejection", (err, promise) => {
   console.log(`Error: ${err.message}`.red.bold);
   // Close server & exit process
-  server.close(() => process.exit(1))
-})
+  server.close(() => process.exit(1));
+});
